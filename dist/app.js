1d9a88f4813660ce9e4550c422d42f9adb3cc33f
joffre <joffre.hermosilla@gmail.com>
2022-02-16 16:53:22 -0500

    Primer commit configuracion de Typescript Node nmp NodeJs server con Typescript

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.nombre = void 0;
+exports.nombre = 'Joffre Hermosilla prueba NodeJS -TypeScript y ReactJS';
+console.log(exports.nombre);
+//# sourceMappingURL=app.js.map
